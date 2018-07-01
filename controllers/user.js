23f9f04b921e51b6dc7664222ebaf9a499bdb57b
wangqiao <2337523303@qq.com>
2018-07-01 21:18:37 +0800

    渲染注册页面

--- controllers/user.js
+++ controllers/user.js
@@ -6,12 +6,14 @@ exports.showSignin = (req,res) => {
 exports.handleSignin = (req,res) => {
     res.send('handleSignIn')
 }
+
 // 渲染注册页面处理函数
 exports.showSignup = (req,res) => {
-    res.send('showSignUp')
+    res.render('signup.html')
 }
 // 处理注册页面处理函数
 exports.handleSignup = (req,res) => {
+    // 渲染注册页面
     res.send('handleSignUp')
 }
 // 处理退出页面处理函数
