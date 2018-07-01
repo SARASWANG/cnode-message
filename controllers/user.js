abdd57e8341f7d54ee76863590d65d9499a172c2
wangqiao <2337523303@qq.com>
2018-07-01 20:45:46 +0800

    用户路由设置完成

---
+++ controllers/user.js
@@ -0,0 +1,20 @@
+// 渲染登录页面处理函数
+exports.showSignin = (req,res) => {
+    res.send('showSignIn')
+}
+// 处理登录页面处理函数
+exports.handleSignin = (req,res) => {
+    res.send('handleSignIn')
+}
+// 渲染注册页面处理函数
+exports.showSignup = (req,res) => {
+    res.send('showSignUp')
+}
+// 处理注册页面处理函数
+exports.handleSignup = (req,res) => {
+    res.send('handleSignUp')
+}
+// 处理退出页面处理函数
+exports.handleSignout = (req,res) => {
+    res.send('handleSignOut')
+}
--- routes/router.js
+++ routes/router.js
@@ -1,11 +1,23 @@
 // 创建路由
-const  express = require('express')
+const express = require('express')
 const router = express.Router()
 // 导入首页路由处理函数模块
 const show = require('../controllers/index.js')
 
+// 导入用户路由处理函数模块
+const user = require('../controllers/user.js')
+
 // 设计路由  index
-router.get('/',show.showIndex)
+router.get('/', show.showIndex)
+
+
+// 用户路由设计
+router
+    .get('/signin', user.showSignin)
+    .post('/signin', user.handleSignin)
+    .get('/signup', user.showSignup)
+    .post('/signup', user.handleSignup)
+    .post('/signout', user.handleSignout)
 
 //导出路由模块
 module.exports = router
